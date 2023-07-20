6ca95495f55b2956a3b5ee9aa62e5c7323b61782
Sai Pradeep Varry <126949128+saipradeepvarry@users.noreply.github.com>
2023-07-21 00:18:57 +0530

    Update FlaskApp.py

--- FlaskApp.py
+++ FlaskApp.py
@@ -6,28 +6,27 @@ twilio
 
 pip install twilio
 
-
-
-from flask import Flask, request
+from flask import Flask,request
 from twilio.twiml.messaging_response import MessagingResponse
 
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-@app.route("/sms", methods=['POST'])
-def sms_reply():
-    """Respond to incoming calls with a simple text message."""
-    # Fetch the message
-    msg = request.form.get('Body')
-
-    # Create reply
+app=Flask(__name__)
+@app.route('/bot',methods=['POST'])
+def bot():
+    incoming_message=request.values.get('Body','').lower()
+    sender_number = request.values.get('From', '')
     resp = MessagingResponse()
-    resp.message("You said: {}".format(msg))
-
+    msg = resp.message()
+    if 'hi' in incoming_message:
+        reply = 'Hi! Are you here to apply internship'
+    elif 'Yes' or 'No':
+        reply='Please Enter Your Name'
+    elif incoming_message.isalpha():
+        reply='Enter your email'
+        if incoming_message.isalpha():
+            reply="Please select how many years of experience you have with Python/JS/ Automation DevelopmentShow List: 1 year/n2years/n3 years/n4 years/n5 years"
+    else:
+        reply='Message: Thanks for connecting. We will getMessage: Thanks for connecting. We will get'
+    
+    msg.body(reply)
+    print(incoming_message,reply)
     return str(resp)
-
-if __name__ == "__main__":
-    app.run(debug=True)
